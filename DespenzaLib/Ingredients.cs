86718486499e5d7afe4e33d3a0fce48164088247
camil <camilla-holck@hotmail.com>
2026-04-20 13:41:56 +0200

    c

--- DespenzaLib/Ingredients.cs
+++ DespenzaLib/Ingredients.cs
@@ -9,7 +9,7 @@ namespace DespenzaLib
     public class Ingredients : Wares
     {
 
-        public decimal GramPrisResultat => KiloPrice / 1000m;
+        public decimal GramPriceResult => KiloPrice / 1000m;
         public Ingredients() : base() { }
 
         public override double GetCost()
