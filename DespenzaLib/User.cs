c6be8cafb6ff1c722db90cdb3ca6d915ab67413e
oliverpollo <olp003@edu.zealand.com>
2026-04-20 10:18:52 +0200

    Ayoo

---
+++ DespenzaLib/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DespenzaLib
+{
+    public class User
+    {
+        private int _userId;
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
