d8f30e0974f432005500efbed65ed8b52708d70b
camil <camilla-holck@hotmail.com>
2026-04-22 20:46:16 +0200

    userform

---
+++ Despenza/Pages/User/Create.cshtml.cs
@@ -0,0 +1,23 @@
+using DespenzaLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Despenza.Pages.User
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Baker Baker { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            DatabaseTemporaryReplacement.Users.Add(Baker);
+
+            return Redirect("/User/AllUsers");
+        }
+    }
+}
