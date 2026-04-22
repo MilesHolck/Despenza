aa49308b72d85829f3493e81a1f0e96fac605d48
oliverpollo <olp003@edu.zealand.com>
2026-04-22 09:52:22 +0200

    DBContext

--- DespenzaLib/Data/AppDbContext.cs
+++ DespenzaLib/Data/AppDbContext.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace DespenzaLib.Data
 {
-    public class AppDbContext 
+    public class AppDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
 
     }
 }
