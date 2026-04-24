ae0ab2e506de830def79ddc5cd936318535d233e
oliverpollo <olp003@edu.zealand.com>
2026-04-24 10:48:11 +0200

    CreateIngredients & Get all.

--- Despenza/Program.cs
+++ Despenza/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IInventoryService, InventoryService>(); 
 
 builder.Services
     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
