59d43a50f296e4d1eafe56b56324c950f840a69f
camil <camilla-holck@hotmail.com>
2026-04-30 20:36:10 +0200

    tilføjet antal knap og property

---
+++ DespenzaLib/Migrations/20260430134246_AddedAntalToRecipe.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DespenzaLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAntalToRecipe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Antal",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Antal",
+                table: "Recipes");
+        }
+    }
+}
