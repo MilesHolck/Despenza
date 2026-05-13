950d2f9606c76cbe8e5e14a3c6fb7e58536b57b5
camil <camilla-holck@hotmail.com>
2026-05-13 13:49:00 +0200

    pris på semiproduct

--- DespenzaLib/Models/SemiProduct.cs
+++ DespenzaLib/Models/SemiProduct.cs
@@ -8,8 +8,8 @@ namespace DespenzaLib.Models
 {
     public class SemiProduct : Wares
     {
-       
-        
+
+
         public int RecipeId { get; set; }
         public Recipe Recipe { get; set; }
         public User User { get; set; }
@@ -17,25 +17,21 @@ namespace DespenzaLib.Models
 
         public SemiProduct()
         {
-            
+
         }
 
         public override decimal GetCost()
         {
-            if (Recipe?.Lines == null || Recipe.OutputQuantity == 0)
+            if (Recipe?.Lines == null || Recipe.QuantityOfProduct == 0)
             {
                 return 0;
             }
 
             var totalCost = Recipe.Lines.Sum(l => l.Quantity * l.Ware.GetCost());
 
-            return totalCost / Recipe.OutputQuantity; 
+            return totalCost / Recipe.QuantityOfProduct;
         }
 
-            //public double AddIngredientPricesTogether()
-            //{
 
-            //}
-
-        }
+    }
 }
