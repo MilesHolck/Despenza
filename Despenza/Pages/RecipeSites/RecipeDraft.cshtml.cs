74168c5beeac76acc42ddcfdd861fd4cdd5dbbd2
camil <camilla-holck@hotmail.com>
2026-05-20 13:29:49 +0200

    halv og helfab synligt

--- Despenza/Pages/RecipeSites/RecipeDraft.cshtml.cs
+++ Despenza/Pages/RecipeSites/RecipeDraft.cshtml.cs
@@ -2,6 +2,7 @@ using DespenzaLib.Data;
 using DespenzaLib.Models;
 using DespenzaLib.Repos;
 using DespenzaLib.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,8 @@ using System.Security.Claims;
 
 namespace Despenza.Pages.RecipeSites
 {
+
+    [Authorize]
     public class RecipeDraftModel : PageModel
     {
         private readonly IRepository<Recipe> _recipeRepo;
